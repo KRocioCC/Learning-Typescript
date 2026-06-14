e917e36c81fc8e195086477f8103693e105c58cf
Karen Calderon <krcatari@umsa.bo>
2026-06-13 20:24:33 -0400

    constantes y variables

---
+++ seccion-3/js/variables_y_constantes.js
@@ -0,0 +1,18 @@
+//CON LET PUEDO CAMBIAR EL VALOR DE LA VARIABLE, PERO CON CONST NO PUEDO CAMBIAR EL VALOR DE LA CONSTANTE
+
+//Let sirve para declarar variables que pueden cambiar su valor
+
+let edad = 30;
+console.log(edad);
+
+edad = 31;
+console.log(edad);
+
+//const sirve para declarar constantes, es decir, variables que no pueden cambiar su valor
+
+const pi = 3.1416;
+console.log(pi);
+
+pi = 3.14; 
+console.log(pi);
+
