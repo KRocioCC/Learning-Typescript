9869a37af822dfe12dbbd21c1a586334d33635db
Karen Calderon <krcatari@umsa.bo>
2026-07-07 10:24:25 -0400

    debanado de strings

---
+++ seccion-3/js/debanado_de_strings.js
@@ -0,0 +1,16 @@
+let cadena = "Estamos validando el debanado de strings";
+
+//para la longitud de la cadena
+console.log(cadena.length);
+
+//metodos
+// console.log(cadena.slice(0,8));
+// console.log(cadena.slice(10));
+
+console.log(cadena.substring(0,20));
+console.log(cadena.substring(34, 2));
+
+//para convertir a mayusculas y minusculas
+console.log(cadena.toUpperCase());
+console.log(cadena.toLowerCase());
+
