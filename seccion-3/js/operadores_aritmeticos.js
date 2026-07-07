5e67b6f1c70f85d0b7b03e6ae635e698a7782983
Karen Calderon <krcatari@umsa.bo>
2026-07-07 10:32:26 -0400

    operadores aritmeticos

---
+++ seccion-3/js/operadores_aritmeticos.js
@@ -0,0 +1,37 @@
+
+const a = 2;
+const b = 5;
+b += 3;
+
+let resultado = a + b;
+console.log(resultado);
+
+resultado = a - b;
+console.log(resultado);
+
+resultado = a * b;
+console.log(resultado);
+
+resultado = a / b;
+console.log(resultado);
+
+resultado = a % b;
+console.log(resultado);
+
+//Para elevar un numero a una potencia se utiliza el operador **
+resultado = a ** b;
+console.log(resultado);
+
+//para modulo
+resultado = a % b;  
+console.log(resultado);
+
+//JERARQUIA DE OPERACIONES
+//parentesis
+//exponenciacion
+//multiplicacion y division
+//suma y resta  
+
+console.log(2 + 3 * 5); // 17
+console.log((2 + 3) * 5); // 25 
+
